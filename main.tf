fd0362212563a2719b47e547ef1df9b40bfcadf8
Abhin86 <achuabhin86@gmail.com>
2024-12-27 15:01:22 +0530

    13

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ module "VPC" {
   source = "./modules/vpc"
   cidr_block = "10.0.0.0/16"
 }
+
+module "subnet_pub" {
+  source = "./modules/subnet"
+  subnet_cidr_block = "10.0.1.0/24"
+  availability_zone = var.availability_zone
+}
