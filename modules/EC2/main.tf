6663e30d731c246a25b4256d936ed3db561b12ce
Abhin86 <achuabhin86@gmail.com>
2024-12-28 14:25:00 +0530

    7

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -385,8 +385,6 @@ resource "aws_spot_instance_request" "this" {
 
   ami                  = local.ami
   instance_type        = var.instance_type
-  cpu_core_count       = var.cpu_core_count
-  cpu_threads_per_core = var.cpu_threads_per_core
   hibernation          = var.hibernation
 
   user_data                   = var.user_data
