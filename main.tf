48fcc2d3311fa970062f02a3f429a52db2610611
Abhin86 <achuabhin86@gmail.com>
2024-12-28 14:17:49 +0530

    8

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "VPC" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   enable_nat_gateway = true
-  azs = ["us-east-1"]
+  azs = ["us-west-1"]
 
 }
 
@@ -16,7 +16,7 @@ module "ec2" {
   name              = "instance"
   instance_type     = "t3.micro"
   ami_ssm_parameter = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-  subnet_id         = module.VPC.public_subnets[0]
+  subnet_id         = module.VPC.public_subnets[1]
   ami =  "ami-0657605d763ac72a8"
   associate_public_ip_address = true
   # key_name          = ""
