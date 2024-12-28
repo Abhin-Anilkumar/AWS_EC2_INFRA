9491f85660752a621f73f308c7e3fa90b1dc67d9
Abhin86 <achuabhin86@gmail.com>
2024-12-28 09:47:54 +0530

    14

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 module "VPC" {
   source = "./modules/vpc"
+  name            = "my-vpc"
+  create_vpc      = true
+  cidr            = "10.0.0.0/16"
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway = true
   
 }
 
