371520b8ba579fc557062632175085049eadcbbc
Abhin86 <achuabhin86@gmail.com>
2024-12-28 10:13:15 +0530

    14

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "ec2" {
   name              = "instance"
   instance_type     = "t3.micro"
   ami_ssm_parameter = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-  subnet_id         = module.VPC.database_subnet_arns
+  subnet_id         = module.VPC.aws_subnet.public[0]
   associate_public_ip_address = true
   key_name          = "abhin.pem"
   tags = {
