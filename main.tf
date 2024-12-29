59be0521e86ddd3d545aad7fde033e09967770ae
Abhin86 <achuabhin86@gmail.com>
2024-12-29 10:45:23 +0530

    11

--- main.tf
+++ main.tf
@@ -10,19 +10,19 @@ module "VPC" {
 
 }
 
-module "ec2" {
-  source = "./modules/EC2"
-  create            = true
-  name              = "instance"
-  instance_type     = "t3.micro"
-  ami_ssm_parameter = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-  subnet_id         = module.VPC.public_subnets[1]
-  ami =  "ami-0657605d763ac72a8"
-  associate_public_ip_address = true
-  # key_name          = ""
-  # monitoring          = true                     # Enable detailed monitoring
-  # disable_api_termination    = true                     # Enable termination protection
-  # iam_instance_profile       = "ec2-iam-role-profile"   # Attach IAM Role
-  # placement_group            = "example-placement-group" 
-  depends_on = [module.VPC]
-}
+# module "ec2" {
+#   source = "./modules/EC2"
+#   create            = true
+#   name              = "instance"
+#   instance_type     = "t3.micro"
+#   ami_ssm_parameter = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+#   subnet_id         = module.VPC.public_subnets[1]
+#   ami =  "ami-0657605d763ac72a8"
+#   associate_public_ip_address = true
+#   # key_name          = ""
+#   # monitoring          = true                     # Enable detailed monitoring
+#   # disable_api_termination    = true                     # Enable termination protection
+#   # iam_instance_profile       = "ec2-iam-role-profile"   # Attach IAM Role
+#   # placement_group            = "example-placement-group" 
+#   depends_on = [module.VPC]
+# }
