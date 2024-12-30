c81e7f21980444abad2e0c922764f1f05d7b1bbb
Abhin86 <achuabhin86@gmail.com>
2024-12-30 10:43:30 +0530

    ww

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "stw_gw" {
 resource "aws_subnet" "stw_subnet_private_1" {
   vpc_id            = aws_vpc.stw_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-1a"
+  availability_zone = "us-west-1c"
 
   tags = {
     Name  = "stw_subnet_private_1"
@@ -40,7 +40,7 @@ resource "aws_subnet" "stw_subnet_public_1" {
   vpc_id                  = aws_vpc.stw_vpc.id
   cidr_block              = "10.0.101.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-west-1c"
   tags = {
     Name  = "stw_subnet_public_1"
   }
